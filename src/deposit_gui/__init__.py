e83dcfaefacfaffb1f6cd48cc484f9dba17673e3
demjanp <peter.demjan@gmail.com>
2022-11-21 16:11:44 +0100

    descriptors no longer displayed as classes in DNavigator

--- src/deposit_gui/__init__.py
+++ src/deposit_gui/__init__.py
@@ -1,8 +1,8 @@
-version_info = (1, 4, 33)
+version_info = (1, 4, 34)
 
 __version__ = '.'.join(map(str, version_info))
 __title__ = 'Deposit'
-__date__ = "11.11.2022"
+__date__ = "21.11.2022"
 
 from deposit_gui.dgui_main import DGUIMain
 
